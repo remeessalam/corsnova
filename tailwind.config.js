11dfc452a8404a992fba985f459ca51045badfc1
remeessalam <remeesreme4u@gmail.com>
2025-03-08 12:15:09 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#fae21c",
+        secondary: "#928202",
+        tertiary: "#928202",
+      },
+      boxShadow: {
+        large: "0px 0px 30px 10px rgba(0, 0, 0, 0.5)",
+      },
+    },
+  },
+  plugins: [],
+};
